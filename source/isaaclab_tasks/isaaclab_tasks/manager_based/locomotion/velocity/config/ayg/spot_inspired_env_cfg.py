5605ed51465a5dbc1a2629e50c48985701dc2da4
Davide De Benedittis <davide.debenedittis@gmail.com>
2026-03-04 15:04:06 +0400

    Add noise corruption to observations

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/ayg/spot_inspired_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/ayg/spot_inspired_env_cfg.py
@@ -104,7 +104,7 @@ class AygObservationsCfg:
         actions = ObsTerm(func=mdp.last_action)
 
         def __post_init__(self):
-            self.enable_corruption = False
+            self.enable_corruption = True
             self.concatenate_terms = True
             self.history_length = 5
             self.flatten_history_dim = True
